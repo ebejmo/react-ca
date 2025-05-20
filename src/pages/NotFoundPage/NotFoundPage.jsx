9f19a1bb416a7c53653fcac2f7b095fb7acc60f9
ebejmo <94139531+ebejmo@users.noreply.github.com>
2025-05-20 19:52:48 +0200

    Fix: Icon size & svg size

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -2,19 +2,18 @@ import { useNavigate } from 'react-router-dom';
 import { goHome } from '../../utils/handlers';
 import UserFeedback from '../../components/UserFeedback/UserFeedback';
 import { SadIcon } from '../../components/Icons';
+import usePageTitle from '../../hooks/usePageTitle';
 
 export default function NotFoundPage() {
+  usePageTitle('Not found');
   const navigate = useNavigate();
-  function handleGoHome() {
-    goHome(navigate);
-  }
 
   return (
     <UserFeedback
       type="error"
       message="Ooops. The page you are looking for does not exist."
-      icon={<SadIcon />}
-      onAction={handleGoHome}
+      icon={<SadIcon className="icon" />}
+      onAction={goHome(navigate)}
       buttonLabel="Go to back to Homepage"
     />
   );
